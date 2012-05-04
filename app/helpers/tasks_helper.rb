b0ec4abdee7391fe608229c41bc40c51800d2fde
cortex <victor.cortes@codescrum.com>
2012-05-04 16:04:57 -0500

    mark date filter link as active

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,10 @@
 module TasksHelper
+
+  def is_active_date?(filter)
+    'active' if params.has_value?(filter.to_s)
+  end
+
+  def is_active_any_date?
+    'active' if !params.has_key?('f')
+  end
 end
