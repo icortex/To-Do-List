8bb8fb59e2b2efc3ab78da54e4f736d2e17ccaa5
Jairo Diaz <jairo.diaz@codescrum.com>
2012-05-22 14:44:43 +0100

    Formated very long lines in file

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -16,11 +16,14 @@ module TasksHelper
     if params[:q]
       case filter
         when :today
-          params.merge(q: {deadline_eq: Date.today, name_cont: params[:q][:name_cont], s: params[:q][:s]}, f: :today).except(:page)
+          params.merge(q: {deadline_eq: Date.today, name_cont: params[:q][:name_cont], s: params[:q][:s]}, 
+                       f: :today).except(:page)
         when :week
-          params.merge(q: {deadline_gteq: Date.today.beginning_of_week, deadline_lteq: Date.today.end_of_week, name_cont: params[:q][:name_cont], s: params[:q][:s]}, f: :week).except(:page)
+          params.merge(q: {deadline_gteq: Date.today.beginning_of_week, deadline_lteq: Date.today.end_of_week, 
+                           name_cont: params[:q][:name_cont], s: params[:q][:s]}, f: :week).except(:page)
         when :month
-          params.merge(q: {deadline_gteq: Date.today.beginning_of_month, deadline_lteq: Date.today.end_of_month, name_cont: params[:q][:name_cont], s: params[:q][:s]}, f: :month).except(:page)
+          params.merge(q: {deadline_gteq: Date.today.beginning_of_month, deadline_lteq: Date.today.end_of_month, 
+                           name_cont: params[:q][:name_cont], s: params[:q][:s]}, f: :month).except(:page)
         when :any
           params[:q].except(:deadline_gteq, :deadline_lteq, :page, :name_cont, :s)
         else
